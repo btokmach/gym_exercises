a5c6c57faa301a667ae5c9fb36b58089d1ff6b03
btokmach <btokmach@gmail.com>
2022-06-13 02:32:58 +0300

    added basic layout for components

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+const HeroBanner = () => {
+  return (
+    <Box sx={{ mt: { lg: '212px', xs: '70px'}, ml: { sm: '50px'}}} position="relative" p="20px">
+        <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+            Fitnes Club
+        </Typography>
+        <Typography>
+            Sweat, Smile <br /> and Repeat
+        </Typography>
+    </Box>
+  )
+}
+
+export default HeroBanner;
