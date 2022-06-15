0f3aff6c80c99a35fa125e00d2df8545fd06bc2d
btokmach <btokmach@gmail.com>
2022-06-15 23:23:15 +0300

    added new components and connected Youtube Search and Download API

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,14 @@ export const exerciseOptions = {
     }
   };
 
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '3ba106329cmsh57a3c8d45af185cp184a13jsndd15c6832955',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }
+};
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
