68aae25dff1551578dce7a737653b124929456ef
btokmach <btokmach@gmail.com>
2022-06-15 04:31:10 +0300

    added card component and pagination

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -6,21 +6,26 @@ import Icon from '../assets/icons/gym.png';
 const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
     <Stack
-        type="button"
-        alignItems="center"
-        justifyContent="center"
-        className="bodyPart-card"
-        sx={{
-            borderTop: bodyPart === item ? '4px solid #ff2625' : '',
-            backgroundColor: '#fff',
-            borderBottomLeftRadius: '20px',
-            width: '270px',
-            height: '280px',
-            cursor: 'pointer',
-            gap: '47px'
-        }}
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={{
+          borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+          backgroundColor: '#fff',
+          borderBottomLeftRadius: '20px',
+          width: '270px',
+          height: '280px',
+          cursor: 'pointer',
+          gap: '47px'
+      }}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({top: 1800, left: 100, behavior: 'smooth'});
+      }}
     >
-        <img src={Icon} alt="dumbbell" styles={{ width: '40px', height: '40px'}} />
+      <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px'}} />
+      <Typography fontSize="24px" fontWeight="bold" color="#3A1212" textTransform="capitalize">{item}</Typography>
     </Stack>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,10 +17,10 @@ const Home = () => {
         bodyPart={bodyPart}
         setBodyPart={setBodyPart} 
       />
-      <Exercises 
+      <Exercises
+        exercises={exercises}
         setExercises={setExercises}
         bodyPart={bodyPart}
-        setBodyPart={setBodyPart} 
       />
     </Box>
   )
